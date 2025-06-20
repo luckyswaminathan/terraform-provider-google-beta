4d6aca0c4d0f77715243b23a3cd3b2083a8fd341
luckyswaminathan <lswamina@google.com>
2025-06-20 09:47:14 -0700

    saving

---
+++ run.sh
@@ -0,0 +1,19 @@
+
+
+## outline of what this script does
+
+## 1. set the release_diff env var to have a non nil value
+
+export release_diff="true"
+
+## 2. github actions to get to correct provider(irrelevant for now)
+
+##3. run make test command for something(hardcoded for now)
+
+make testacc TEST=./google-beta/services/bigtable TESTARGS='-run=TestAccBigtableInstance_basic' > output.log
+
+## 4. now that we have an output file(given changes to vcr), parse it and run grep FLAG to get all diff tests
+
+## 5. print those diffs
+
+unset release_diff
